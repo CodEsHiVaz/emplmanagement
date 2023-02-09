57cbe87223c5debfb7941331ed77809ca34a64f4
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2023-02-09 17:37:12 +0530

    file solved

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 app.use(cors());
 app.set("view engine", "hbs");
 app.set("view engine", "html");
@@ -21,13 +22,9 @@ hbs.registerHelper("isfirst", function (value) {
   return value !== 0;
 });
 
-
-
 hbs.registerHelper("isMarraied", function (value) {
   return value == "Married";
 });
-
-
 
 hbs.registerHelper("isUnmarraied", function (value) {
   return value == "Unmarried";
@@ -36,14 +33,13 @@ hbs.registerHelper("isUnmarraied", function (value) {
 hbs.registerPartials(path.join(__dirname, "./views/partials"), function (err) {
   if (err) console.log("partial error", err);
 });
-// console.log(path.join(__dirname, "./views/partials"));
+console.log(path.join(__dirname, "./uploads"));
 app.use(homeRouter);
 
- 
 app.listen(port, async () => {
   try {
     await connect_db();
-    console.log("server listening on port 8080");
+    console.log("server listening on http://localhost:8080/index");
   } catch (error) {
     console.log("server listening failed deu to", error.message);
   }
